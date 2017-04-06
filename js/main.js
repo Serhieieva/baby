6a3a7ae980e8b3e1741f6e81add40263ce247d85
Hex <Sergeeva.Anastacia@gmail.com>
2017-04-06 15:31:30 +0300

    Logic for resize added.

--- js/main.js
+++ js/main.js
@@ -14,26 +14,59 @@ tryBackgroundImages('content-background', $('.container'));
 
 $(window).on('load resize', function () {
     var eventPlace = document.getElementById('event_venue');
-    var winHeight = $(window).height();
-    var winWidth = $(window).width();
-    var widthToHeightRatio = 1.338;
-    var wrapDiv = $('.wrapDiv');
+
+    resizeWrapper();
+
+    // wrapDiv.css({'height': wrapDiv.width() * widthToHeightRatio});
     //
-    // if (wrapDiv.height() > winHeight) {
-    //     wrapDiv.width(winHeight / 1.345);
-    // }
+    // $('.container').css({'font-size': wrapDiv.width() / 23 + 'px'});
 
-    wrapDiv.css({'height': wrapDiv.width() * widthToHeightRatio});
-
-    $('.container').css({'font-size': wrapDiv.width() / 23 + 'px'});
-   eventPlace.innerHTML = removeUSFromAddress(eventPlace.innerHTML);
-
-    console.log(wrapDiv.height(), winHeight);
+    eventPlace.innerHTML = removeUSFromAddress(eventPlace.innerHTML);
 });
+
+function resizeWrapper (wrapDiv) {
+    var winHeight = $(window).height(),
+        widthToHeightRatio = 1.337,
+        heightToWidthRatio = 0.75,
+        wrapDiv = $('.wrapDiv'),
+        wpapperHeight,
+        wpapperWidth;
+
+    if ($(window).width() > 550 && winHeight > 736) {
+        wpapperHeight = 736;
+        wpapperWidth = 550;
+    } else if ($(window).width() > 550) {
+        wpapperHeight = winHeight;
+        wpapperWidth = wpapperHeight * heightToWidthRatio;
+    } else if (winHeight > 736) {
+        wpapperWidth = $(window).width();
+        wpapperHeight = wpapperHeight * widthToHeightRatio;
+    } else {
+        if ($(window).width() / winHeight < 1.338) {
+            wpapperHeight = ($(window).width() - 30) * widthToHeightRatio;
+            wpapperWidth = wpapperHeight * heightToWidthRatio;
+        } else {
+            wpapperHeight = winHeight;
+            wpapperWidth = winHeight * heightToWidthRatio;
+        }
+    }
+
+    if ($(window).width() < 320 || winHeight < 400) {
+        wpapperHeight = 388;
+        wpapperWidth = 290;
+    }
+
+    wrapDiv.css({
+        'height': wpapperHeight,
+        'width': wpapperWidth
+    });
+
+    $('.container').css({'font-size': wpapperWidth / 23 + 'px'});
+}
 
 function removeUSFromAddress (address) {
     var regexp = /,*\s*usa|,*\s*US|,*\s*united states( of america)?/i;
-    return address.replace(regexp,' ');
+    return address.replace(regexp, ' ');
 }
 
 function tryBackgroundImages (name, destination) {
